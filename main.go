fcb0f2c9ab9618ca71a395dc46789eddc0fc2a6e
Denis Borzenko <den.b.bars@gmail.com>
2022-08-06 00:14:51 +0300

    Change --maxTime parameter (int->duration)

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"time"
 )
 
 func corsMiddleware(w http.ResponseWriter, r *http.Request, allowOrigin string, next http.HandlerFunc) {
@@ -19,25 +18,25 @@ func main() {
 	listenPtr := flag.String("listen", ":80", "HTTP listen [host]:port")
 	engineBinPtr := flag.String("engineBin", "./stockfish/stockfish_15_linux_x64", "UCI engine binary path")
 	allowOriginPtr := flag.String("allowOrigin", "*", "Value for HTTP header Access-Control-Allow-Origin")
-	maxTimePtr := flag.Int("maxTime", 0, "Max time limit (ns)")
+	maxTimePtr := flag.Duration("maxTime", 0, "Max time limit (ns)")
 	helpPtr := flag.Bool("help", false, "Show usage info")
-	
+
 	flag.Parse()
 	if *helpPtr {
 		flag.Usage()
 		return
 	}
-	
+
 	log.Println("listen", *listenPtr)
 	log.Println("engineBin", *engineBinPtr)
 	log.Println("allowOrigin", *allowOriginPtr)
 	log.Println("maxTime", *maxTimePtr)
-	
-	uciHttpHandlers := NewUciHttpHandlers(*engineBinPtr, time.Duration(*maxTimePtr))
-	
-	http.HandleFunc("/go", func (w http.ResponseWriter, r *http.Request) {
+
+	uciHttpHandlers := NewUciHttpHandlers(*engineBinPtr, *maxTimePtr)
+
+	http.HandleFunc("/go", func(w http.ResponseWriter, r *http.Request) {
 		corsMiddleware(w, r, *allowOriginPtr, uciHttpHandlers.HandleGo)
 	})
-	
+
 	log.Fatal(http.ListenAndServe(*listenPtr, nil))
 }
